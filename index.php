dd4c39469a816b36fb2eac2b964a73b3194ac377
DevnathNS <92820691+DevnathNS@users.noreply.github.com>
2024-04-17 13:30:19 +0530

    Update index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <h1>Welcome to Ayurveda Lifestyle Web App</h1>
+    <p>Please <a href="login.php">login</a> or <a href="register.php">register</a> to get started.</p>
+</body>
+</html>
