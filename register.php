80d0486c2e6385abe3929173d36e0a94027abb69
Devnath N S <nsdevnath172@localhost.localdomain>
2024-04-17 14:20:32 +0530

    .

--- register.php
+++ register.php
@@ -6,6 +6,7 @@ if(isset($_SESSION['user_id'])) {
 }
 include('db_config.php');
 
+// Registration form submission
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -13,8 +14,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $query = "INSERT INTO users (username, password, email, role) VALUES ('$username', '$password', '$email', 'user')";
     if(mysqli_query($conn, $query)) {
-        header("Location: login.php");
-        exit;
+        $success_message = "Registered successfully. You can now <a href='login.php'>login</a>.";
     } else {
         $error = "Registration failed";
     }
@@ -32,6 +32,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         <h2>Register</h2>
         <?php if(isset($error)) { ?>
             <p><?php echo $error; ?></p>
+        <?php } ?>
+        <?php if(isset($success_message)) { ?>
+            <p><?php echo $success_message; ?></p>
         <?php } ?>
         <form method="post">
             <input type="text" name="username" placeholder="Username" required>
