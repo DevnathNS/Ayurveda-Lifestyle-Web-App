40f508569e28b63524bf352d1655729ee9619adc
DevnathNS <92820691+DevnathNS@users.noreply.github.com>
2024-04-23 21:45:44 +0530

    Update pitta_recipes.php

--- user/pitta_recipes.php
+++ user/pitta_recipes.php
@@ -8,6 +8,24 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        .carousel-item img {
+            width: 100%;
+            height: auto;
+            max-height: 400px; /* Adjust max-height as needed */
+            object-fit: cover;
+        }
+
+        .modal-body img {
+            max-width: 100%;
+            height: auto;
+            object-fit: contain;
+        }
+
+        .modal-content {
+            padding: 20px;
+        }
+    </style>
 </head>
 <body>
 
@@ -57,7 +75,6 @@
     </div>
 </div>
 
-<!-- Recipe Modals -->
 <div class="modal fade" id="recipeModal1" tabindex="-1" role="dialog" aria-labelledby="recipeModal1Label" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -68,7 +85,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <img src="pitta_recipe1_full.jpg" class="d-block mx-auto mb-3" alt="Recipe 1 Full">
+                <img src="pitta_recipe1.jpg" class="d-block mx-auto mb-3" alt="Recipe 1 Full">
                 <p>Full Recipe 1 details here...</p>
             </div>
             <div class="modal-footer">
@@ -88,7 +105,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <img src="pitta_recipe2_full.jpg" class="d-block mx-auto mb-3" alt="Recipe 2 Full">
+                <img src="pitta_recipe2.jpg" class="d-block mx-auto mb-3" alt="Recipe 2 Full">
                 <p>Full Recipe 2 details here...</p>
             </div>
             <div class="modal-footer">
@@ -108,7 +125,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <img src="pitta_recipe3_full.jpg" class="d-block mx-auto mb-3" alt="Recipe 3 Full">
+                <img src="pitta_recipe3.jpg" class="d-block mx-auto mb-3" alt="Recipe 3 Full">
                 <p>Full Recipe 3 details here...</p>
             </div>
             <div class="modal-footer">
