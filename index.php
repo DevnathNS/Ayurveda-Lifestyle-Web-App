6166fec774b9112edf2599f651761ad62ab69df1
Devnath N S <nsdevnath172@localhost.localdomain>
2024-05-14 21:25:27 +0530

    added styles

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ require_once('session_start.php');
 <html>
 <head>
     <title>Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    <h1>Welcome to Ayurveda Lifestyle Web App</h1>
-    <p>Please <a href="login.php">login</a> or <a href="register.php">register</a> to get started.</p>
+    <div class="container">
+        <h1 class="mt-5">Welcome to Ayurveda Lifestyle Web App</h1>
+        <p class="mt-3">Please <a href="login.php">login</a> or <a href="register.php">register</a> to get started.</p>
+    </div>
 </body>
 </html>
